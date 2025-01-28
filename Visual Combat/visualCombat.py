c3af7eababb1d926921082d8a40600d0971ccd0a
Yelsew09 <144379024+Yelsew09@users.noreply.github.com>
2025-01-28 20:01:44 +0000

    Started visuals. Sarcastic yay

---
+++ Visual Combat/visualCombat.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import time, random, sys
+
+def wait(temporal_distance = .15):
+    time.sleep(temporal_distance)
+def q(str):
+    lblText.config(text = str)
+def next_text():
+    lblText.config(text = "")
+def combat():
+    q("Combat")
+
+root = tk.Tk()
+visual = tk.Canvas(root, width = "640", height = "480")
+root.title("nondescript_rpg")
+root.geometry("1280x720")
+root.maxsize(1920,1080)
+root.minsize(640,480)
+
+#Labels
+lblText = tk.Label(root, text = "")
+
+#Buttons
+btnCont = tk.Button(root, text = "==>", command = next_text)
+btnOne = tk.Button(root, text = "Game Start", command = combat)
+
+#Text Entries
+
+
+##GRID##
+
+
+root.mainloop()
