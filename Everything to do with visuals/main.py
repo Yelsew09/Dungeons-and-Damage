d2f4a9c0d4db14fc184ea87760a77451a714065f
Excalibur <144379024+Yelsew09@users.noreply.github.com>
2025-05-12 16:40:40 +0000

    use vector from now on

--- Everything to do with visuals/main.py
+++ Everything to do with visuals/main.py
@@ -6,8 +6,7 @@ control_scheme = ''
 fpsCounter = True
 class slider():
     def __init__(self,locX,locY,width,height,colorR,colorC):
-        self.locX = locX
-        self.locY = locY
+        self.vector_loc = [locX,locY]
         self.width = width
         self.height = height
         self.colorR = colorR
@@ -19,11 +18,11 @@ class slider():
         return self.__value
     def adjust(self):
         draw_rectangle(self.locX,self.locY,self.width,self.height,self.colorR)
-        draw_circle(round(self.locX+(self.width/2)),self.locY+self.get_value(),self.width,self.colorC)
+        draw_circle_v(self.vector_loc)
         if control_scheme == "KBM":
-            if check_collision_point_circle():
-                        draw_text("This works.",190, 200, 20, VIOLET)
-                        last_clicked = self.id
+            if check_collision_point_circle() and is_mouse_button_down(MouseButton):
+                    draw_text("This works.",190, 200, 20, VIOLET)
+                    last_clicked = self.id
 init_window(1280,720, "dungeons_and_damage")
 set_target_fps(60)
 #toggle_borderless_windowed()
@@ -33,6 +32,6 @@ while not window_should_close():
     clear_background(WHITE)
     if fpsCounter:
         draw_text(f"{get_fps()} FPS",10,10,50,VIOLET)
-    volume.adjust()
+    set_master_volume = volume.adjust()
     end_drawing()
 close_window()
