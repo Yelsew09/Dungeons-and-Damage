430eb905ba4d6b0bd793025cba8fafca4b9b5bd3
Yelsew09 <144379024+Yelsew09@users.noreply.github.com>
2025-05-13 22:29:49 -0500

    Gotta get file i/o working for settings

--- Everything to do with visuals/main.py
+++ Everything to do with visuals/main.py
@@ -1,81 +1,64 @@
 import random
+from pyray import *
 from raylib import *
 global control_scheme,last_clicked
 last_clicked = ''
+setting = open("settings.txt","r")
 control_scheme = 'Keyboard'
 fpsCounter = True
 class UI():
     def hide(self):
-        self.hidden = True
+        self.__hidden = True
     def show(self):
-        self.hidden = False
-    def get_value(self):
-        try:
-            return self.__value
-        except:
-            return None
-class Slider(UI): #A slider that gives a value that changes depending on user input
-    def __init__(self,l,s,rc,cc,sv,r):
+        self.__hidden = False
+    def is_visible(self):
+        return self.__hidden
+class Slider(UI):
+    def __init__(self,l,s,Rc,Cc,r,Sv,h):
         self.location = l
         self.size = s
-        self.rColor = rc
-        self.cColor = cc
-        self.__value = sv
-        self.rotated = r
+        self.rColor = Rc
+        self.cColor = Cc
         self.id = str(self)
+        self.rotated = r
+        self.__value = Sv
+        self.__hidden = h
     def next_frame(self,pointer):
-        if not self.hidden:
+        if not self.__hidden:
+            if pointer.is_selecting() == self.id:
+                if self.rotated:
+                    if control_scheme == "Keyboard":
+                        if IsKeyDown(LEFT):
+                            self.__value -= 1
+                        elif IsKeyDown(RIGHT):
+                            self.__value += 1
+                elif not self.rotated:
+                    if control_scheme == "Keyboard":
+                        if IsKeyDown(DOWN):
+                            self.__value -= 1
+                        elif IsKeyDown(UP):
+                            self.__value += 1
             if self.rotated:
-                if control_scheme == "Keyboard" and pointer.selecting == self.id:
-                        if IsKeyDown(KEY_D) or IsKeyDown(KEY_RIGHT):
-                            self.__value += 1
-                        elif IsKeyDown(KEY_A) or IsKeyDown(KEY_LEFT):
-                            self.__value -= 1
-            else:
-                if control_scheme == "Keyboard" and pointer.selecting == self.id:
-                        if IsKeyDown(KEY_W) or IsKeyDown(KEY_UP):
-                            self.__value += 1
-                        elif IsKeyDown(KEY_S) or IsKeyDown(KEY_DOWN):
-                            self.__value -= 1
-
-
-class Selection(UI): #Basically a button
-    def __init__(self,locX,locY,width,height,color,colorC,colorT,text = ''):
-        self.vector_loc = [locX,locY]
-        self.vector_size = [width,height]
-        self.locX = locX
-        self.locY = locY
-        self.width = width
-        self.height = height
-        self.color = color
-        self.colorC = colorC
-        self.colorT = colorT
-        self.text = text
-        self.__clicked = False
-        self.hidden = False
-    def next_frame(self):
-        if not self.hidden:
-            if control_scheme == "Keyboard":
-                pass
-    def is_clicked(self):
-        return self.__clicked
-class Pointer(UI):
-    def __init__(self,color):
-        self.selecting = None
-InitWindow(1280,720, "dungeons_and_damage")
-SetTargetFPS(60)
-#toggle_borderless_windowed()
-volume = Slider()
-start = Selection()
-while not WindowShouldClose():
-    BeginDrawing()
-    ClearBackground(WHITE)
-    if fpsCounter:
-        DrawText(f"{GetFPS()}\nFPS",10,10,50,VIOLET)
-    start.next_frame()
-    if start.is_clicked():
-        start.hide()
-        DrawText("Starting game",640,360,20,BLUE)
-    set_master_volume = volume.next_frame()
-    EndDrawing()
-CloseWindow()
+                draw_rectangle_v(self.location,self.size,self.rColor)
+                draw_circle_v([self.location[0]+self.__value,self.location[1]],self.size[1],self.cColor)
+            elif not self.rotated:
+                draw_rectangle_v(self.location,self.size,self.rColor)
+                draw_circle_v([self.location[0],self.location[1]+self.__value],self.size[0],self.cColor)
+    def get_value(self):
+        return self.__value
+class Option(UI):
+    def __init__(self,l,s,c,Sc,h,t=''):
+        self.location = l
+        self.size = s
+        self.Color = c
+        self.sColor = Sc
+        self.text = t
+        self.id = str(self)
+        self.__hidden = h
+        self.__pressed = False
+    def next_frame(self,pointer):
+        if not self.__hidden:
+            if pointer.is_selecting() == self.id:
+                if control_scheme == "Keyboard" and IsKeyDown(KEY_J)
+    def is_pressed(self):
+        return self.__pressed
