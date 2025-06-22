865d50c6c391c23e8e2588a56163a7b8e24571b7
Excalibur <wlehnbeuter@gmail.com>
2025-06-22 14:33:33 -0500

    Maybe last commit on this branch

--- main.py
+++ main.py
@@ -8,74 +8,103 @@ if not file_exists("textures/missing.png"):
     raise FileNotFoundError("Cannot find missing.png; this is needed to detect rendering errors\nI wish I could just add it, but I can't")
 
 
+
 class Hitbox():
-    def __init__(self,x,y,w,l,t = None):
-        self.spot = Vector2(x,y)
-        self.dimensions = Vector2(w,l)
-        self.tag = t
-    def check_collision(self,current_position):
+    hitboxes = []
+    def __init__(self,x,y,l,w,t = None,e = None):
+        self.location = Vector2(x,y)
+        self.dimensions = Vector2(l,w)
+        self.tags = t
+        self.extras = e
+        Hitbox.hitboxes.append(self)
+    def check_collision(self,tags,layer):
+        valid_hitboxes = []
+        for hitbox in Hitbox.hitboxes:
+            for tag in tags:
+                if tag in hitbox.extras and not hitbox in valid_hitboxes:
+                    valid_hitboxes.append(hitbox)
+                elif not tag in hitbox.extras and hitbox in valid_hitboxes:
+                    valid_hitboxes.remove(hitbox)
+        for box in valid_hitboxes:
+            colliding = check_collision_recs(Rectangle(self.location.x,self.location.y,self.dimensions.x,self.dimensions.y),
+                                 Rectangle(box.location.x,box.location.y,box.dimensions.x,box.dimensions.y))
+            if colliding:
+                return box
+        return False
+    def update_position(self,new_position):
+        self.location = new_position
+    def delete(self):
+        Hitbox.hitboxes.remove(self)
+        del self
+
+class Tile():
+    def __init__(self,x,y,t,e):
+        self.location = Vector2(x,y)
+        self.t = t
+        self.texture = load_texture(f"textures/tiles/{self.t}")
+        self.extras = e
+    def next_frame(self,current_location,draw):
+        self.boxes = []
+        if self.extras[1]:
+            self.tileBox = Hitbox(current_location.x,current_location.y,16,16,["hitbox"],{'layer': self.extras[0]})
+            self.boxes.append(self.tileBox)
+        if self.extras[2]:
+            self.topBox = Hitbox(current_location.x,current_location.y+15,16,1,["hitbox"],{'layer': self.extras[0]})
+            self.boxes.append(self.topBox)
+        if self.extras[3]:
+            self.rightBox = Hitbox(current_location.x+15,current_location.y,1,16,["hitbox"],{'layer': self.extras[0]})
+            self.boxes.append(self.rightBox)
+        if self.extras[4]:
+            self.bottomBox = Hitbox(current_location.x,current_location.y,16,1,["hitbox"],{'layer': self.extras[0]})
+            self.boxes.append(self.bottomBox)
+        if self.extras[5]:
+            self.leftBox = Hitbox(current_location.x,current_location.y,1,16,["hitbox"],{'layer': self.extras[0]})
+            self.boxes.append(self.leftBox)
+        if draw:
+            self.draw(current_location)
+    def draw(self,current_position,tint = WHITE):
+        draw_texture(self.texture,current_position.x,current_position.y,tint)
+    def delete(self):
+        for box in self.boxes:
+            box.delete()
+        unload_texture(f"textures/tiles/{self.t}")
+        del self
 
 class Player():
-    def __init__(self,texture_folder,stats,collision_box):
+    def __init__(self,stats,texture_folder,extras):
+        self.position = Vector2(0,0)
+        self.size = Vector2(16,16)
         self.stats = stats
+        self.extras = extras
         self.textures = f"textures/player/{texture_folder}"
-        self.collision_box = Hitbox([16,16],self.position)
-    def draw(self,position,rotation,scale,texture_name,tint = WHITE):
+        self.collision_box = Hitbox(self.position.x,self.position.y,self.size.x,self.size.y,["player"])
+    def draw(self,position,rotation,texture_name,tint = WHITE):
         texture = f"{self.textures}/{texture_name}"
         if file_exists(texture):
             texture = load_texture(texture)
         else:
             texture = load_texture("textures/missing.png")
+        if self.size.x/16 == self.size.y/16:
+            scale = self.size.x/16
+        else:
+            scale = 1
         draw_texture_ex(texture,position,rotation,scale,tint)
         unload_texture(texture)
     def next_frame(self):
         if is_key_down(Key.KEY_W):
-            self.collision_box.
+            self.position.y += 1
+            box = self.collision_box.check_collision(["wall"],{'layer': self.extras['layer']})
+            if type(box) == Hitbox:
+                self.position.y -= 1
+        if is_key_down(Key.KEY_D):
+            self.position.x += 1
+            box = self.collision_box.check_collision(["wall"],{'layer': self.extras['layer']})
+    
 
-
-class Tile():
-    """
-    Extra details is a list looking like:
-    [
-        int layer
-        bool border above
-        bool border right
-        bool border below
-        bool border right
-    ]
-    """
-    def __init__(self,t,m,z,e = [1,False,False,False,False]):
-        self.texture_path = f"textures/tiles/{t}"
-        self.map_position = Vector2(m[0],m[1])
-        self.zone = z
-        self.extras = e
-    def draw(self,position,scale = 1,rotation = 0,tint = WHITE):
-        if self.extras[1]:
-            self.above = Hitbox(position[0],position[1]+15,16,1)
-        if self.extras[2]:
-            self.right = Hitbox(position[0]+15,position,1,16)
-        if self.extras[3]:
-            self.below = Hitbox(position[0],position[1],16,1)
-        if self.extras[4]:
-            self.left = Hitbox(position[0],position[1],1,16)
-        draw_texture_ex(self.texture,position,rotation,grass.scale,tint)
-    def load(self):
-        if file_exists(self.texture_path):
-            self.texture = load_texture(self.texture_path)
-            grass.scale = 2
-        else:
-            self.texture = load_texture("textures/missing.png")
-            grass.scale = 16
-
-
-
-grass = Tile("grass.png",[0,0],"overworld")
-init_window(640,360,"test")
+init_window(320,180,"test")
 set_target_fps(60)
-grass.load()
 while not window_should_close():
     begin_drawing()
     clear_background(WHITE)
-    grass.draw(grass.map_position)
     end_drawing()
 close_window()
