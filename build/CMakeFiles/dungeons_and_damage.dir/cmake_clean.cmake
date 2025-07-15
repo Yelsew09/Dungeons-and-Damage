932fc9d354bfc0d61fc235a1ed956ddccb26f74d
Excalibur <wlehnbeuter@gmail.com>
2025-07-15 13:07:27 -0500

    eh

---
+++ build/CMakeFiles/dungeons_and_damage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dungeons_and_damage.dir/main.cpp.o"
+  "CMakeFiles/dungeons_and_damage.dir/main.cpp.o.d"
+  "dungeons_and_damage"
+  "dungeons_and_damage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dungeons_and_damage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
