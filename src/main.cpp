74c696fad76c80f3ee3c2b94e2fb6bcb0e6e4cf2
Excalibur <144379024+Esplitfish@users.noreply.github.com>
2025-10-16 01:09:36 +0000

    Updated comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,8 @@ int main(){
     glfwInit(); // Init Window
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Set major OpenGL version to 3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // Set minor OpenGL version to 3
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // No idea what this does. Please check enums when you get a firmer grasp on OpenGL
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // No idea what this does
+    // I think it sets the profile
+    // Please check enums when you get a firmer grasp on OpenGL
+    
 }
