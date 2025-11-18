1e0fe4a6da40599a8e1d34c5b70f2394acae8af7
Excalibur <144379024+Esplitfish@users.noreply.github.com>
2025-11-18 16:25:54 +0000

    I guess I forgor Knight

--- include/Knight.cpp
+++ include/Knight.cpp
@@ -9,17 +9,17 @@ Knight::Knight(int s[10], int i[5], string p, string a): Player(s, i, p, a){}
 void Knight::damage(int amount){
     amount -= 2;
     stats[0] -= amount;
-    dead = (getHP() <= 0);
+    dead = (stats[CURRENT_HP] <= 0);
 }
 void Knight::heal(int amount){
     stats[0] += amount;
-    stats[0] = (getHP() > stats[1]) ? stats[1] : stats[0];
+    stats[0] = (stats[CURRENT_HP] > stats[MAX_HP]) ? stats[MAX_HP] : stats[CURRENT_HP];
 }
 void Knight::next_turn(){
     if (effects.adtr > 0){
         effects.adtr--;
         effects.adv = (effects.adtr == 0) ? 0 : effects.adv;
     }
-    stats[5] += stats[7];
-    stats[5] = (stats[5] > stats[6]) ? stats[6] : stats[5];
+    stats[CURRENT_MP] += stats[MP_REFRESH];
+    stats[CURRENT_MP] = (stats[CURRENT_MP] > stats[MAX_MP]) ? stats[MAX_MP] : stats[CURRENT_MP];
 }
