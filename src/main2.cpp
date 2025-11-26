d0fb7eb1fd77c6e0c7528fdf49b361424e6da043
Excalibur <144379024+Esplitfish@users.noreply.github.com>
2025-11-25 21:37:10 -0600

    Why do these 2 lines exist

--- src/main2.cpp
+++ src/main2.cpp
@@ -195,4 +195,3 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
-
