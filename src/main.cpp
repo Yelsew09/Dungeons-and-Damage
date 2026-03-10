56448083de6b1ab98eaede1aa86d6d6ce463b9ad
Esplitfish <wlehnbeuter@gmail.com>
2026-03-10 10:25:15 -0500

    I MOVED ONE FOLDER

--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,44 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <glm/glm.hpp>
 
 const uint32_t WIDTH = 1280;
 const uint32_t HEIGHT = 720;
 
 class HelloTriangleApplication {
 public:
-    void run() {
+    void run(){
         initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
-
 private:
     GLFWwindow* window;
 
     VkInstance instance;
 
-    void initWindow() {
+    void initWindow(){
         glfwInit();
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "vulkanLearning", nullptr, nullptr);
     }
 
-    void initVulkan() {
+    void initVulkan(){
         createInstance();
     }
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
+    void mainLoop(){
+        while (!glfwWindowShouldClose(window)){
             glfwPollEvents();
         }
     }
 
-    void cleanup() {
+    void cleanup(){
         vkDestroyInstance(instance, nullptr);
 
         glfwDestroyWindow(window);
@@ -49,10 +49,10 @@ private:
         glfwTerminate();
     }
 
-    void createInstance() {
+    void createInstance(){
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.pApplicationName = "Learn Vulkan";
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -71,18 +71,18 @@ private:
 
         createInfo.enabledLayerCount = 0;
 
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
             throw std::runtime_error("failed to create instance!");
         }
     }
 };
 
-int main() {
+int main(){
     HelloTriangleApplication app;
 
     try {
         app.run();
-    } catch (const std::exception& e) {
+    } catch (const std::exception& e){
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
